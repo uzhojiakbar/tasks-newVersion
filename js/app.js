df1676cbb9e7483bb5f5dcc0eee8ba5c5dde6af4
uzhojiakbar <uzhojiakbar3@gmail.com>
2024-06-24 01:24:52 +0500

    Project done

--- js/app.js
+++ js/app.js
@@ -2,6 +2,184 @@ let siteBody = document.getElementById("body");
 isLogin = false;
 page = "/login";
 
+let base = [
+  { id: 1, name: "Html", status: false, isEdited: false },
+  { id: 2, name: "JavaScript", status: false, isEdited: false },
+  { id: 3, name: "React Js", status: false, isEdited: false },
+  { id: 4, name: "React", status: false, isEdited: false },
+];
+
+const bodyTasks = () => {
+  let add = "";
+  let save = "";
+
+  const siteBody = document.getElementById("body");
+
+  siteBody.innerHTML = `
+  <div>
+    <h1 id="titleInnerBase" class="task_count">${base.length} Tasks</h1>
+  </div>
+
+  <div class="mainDate" id="taskList"></div>
+  <div class="addWrap">
+      <input type="text" id="addInput" placeholder="Add Task" />
+      <button class="task-delete task-edit" id="addBtn">
+      <i class="fa-solid fa-plus"></i>
+      </button>
+  </div>
+`;
+
+  const generic = (state, action) => {
+    switch (action.type) {
+      // Check
+      case "check": {
+        return state.map((value) => {
+          return value.id == action.payload
+            ? { ...value, status: !value.status }
+            : value;
+        });
+      }
+
+      // Delete
+      case "delite": {
+        const innerbase = state.filter((v) => v.id !== action.payload);
+        base = innerbase;
+        document.getElementById(
+          "titleInnerBase"
+        ).innerText = `${base.length} Tasks`;
+        return innerbase;
+      }
+
+      // Search
+      case "search": {
+        return base.filter((v) =>
+          v.name.toLowerCase().includes(action.payload.toLowerCase())
+        );
+      }
+
+      // Add
+      case "add": {
+        const innerData11 = [
+          ...state,
+          {
+            id: state.length ? state[state.length - 1].id + 1 : 1,
+            name: action.payload,
+            status: false,
+            isEdited: false,
+          },
+        ];
+
+        document.getElementById(
+          "titleInnerBase"
+        ).innerText = `${innerData11.length} Tasks`;
+        return innerData11;
+      }
+
+      // Edit
+      case "edit": {
+        return state.map((value) => {
+          if (value.id == action.payload) {
+            save = value.name;
+            return { ...value, isEdited: !value.isEdited };
+          } else return value;
+        });
+      }
+
+      // Save
+      case "save": {
+        return state.map((value) => {
+          if (value.id == action.payload) {
+            return { ...value, name: save, isEdited: false };
+          } else return value;
+        });
+      }
+
+      default:
+        return state;
+    }
+  };
+
+  const dispatch = (action) => {
+    base = generic(base, action);
+    render();
+  };
+
+  const render = () => {
+    if (!base.length) {
+      document.getElementById("taskList").innerHTML = `
+            <div class="not-found">
+              <img class="notfound_icon" src="./img/search.gif" alt="">
+              <h2 class="notfound-txt">
+                  Hech qanday malumot mavjud emas
+              </h2>
+        </div> 
+        `;
+    } else
+      document.getElementById("taskList").innerHTML = base
+        .map(
+          (value) => `
+        <div class="task-item">
+          <input type="checkbox" class="checkck" ${
+            value.status ? "checked" : ""
+          } onchange="toggleCheck(${value.id})" />
+          ${
+            value.isEdited
+              ? `
+            <input class="editInput" type="text" value="${value.name}" oninput="updateSave(event)" />
+            <button class="task-delete task-edit" onclick="saveTask(${value.id})">
+            <i class="fa-solid fa-floppy-disk"></i>
+            </button>
+            <button class="task-delete" onclick="cancelEdit(${value.id})">
+            <i class="fa-solid fa-xmark"></i>
+            </button>
+          `
+              : `
+            <span ${
+              value.status
+                ? 'style="text-decoration: line-through;color: #838383;"'
+                : ""
+            } class="textItem" >${value.name}</span>
+
+
+            <button class="task-delete task-edit" onclick="editTask(${
+              value.id
+            })">
+            <i class="fa-solid fa-pen-to-square"></i>
+            </button>
+            <button class="task-delete" onclick="deleteTask(${value.id})">
+            <i class="fa-solid fa-trash"></i>
+            </button>
+          `
+          }
+        </div>
+      `
+        )
+        .join("");
+  };
+
+  document.getElementById("addBtn").addEventListener("click", () => {
+    if (add.trim() !== "") {
+      dispatch({ type: "add", payload: add });
+      add = "";
+      document.getElementById("addInput").value = "";
+    } else {
+      alert("Malumot kiriting!");
+    }
+  });
+
+  document.getElementById("addInput").addEventListener("change", (event) => {
+    add = event.target.value;
+  });
+
+  window.toggleCheck = (id) => dispatch({ type: "check", payload: id });
+  window.editTask = (id) => dispatch({ type: "edit", payload: id });
+  window.saveTask = (id) => dispatch({ type: "save", payload: id });
+  window.cancelEdit = (id) => dispatch({ type: "edit", payload: id });
+  window.deleteTask = (id) => dispatch({ type: "delite", payload: id });
+  window.updateSave = (event) => (save = event.target.value);
+
+  render();
+};
 const changePage = (pageText) => {
   try {
     page = pageText;
@@ -15,29 +193,7 @@ const changePage = (pageText) => {
 var getData = () => {
   error = "Kirish";
   if (isLogin) {
-    console.log("LOOG:", tasks);
-    siteBody.innerHTML = ` 
-    
-    `;
-    // tasks.map((value) => {
-    //   if (dataName) {
-    //     dataName.innerHTML = tasks
-    //       .map(
-    //         (value) => `;
-    //           <div id="data-item" class="data-item">
-    //               <div class="task-name">
-    //                   <input onclick="remain_count(checkbox)" id="${value.id}" class="data-check" type="checkbox" id="">
-    //                   <label class="dataname" id="data-name" for="${value.id}">
-    //                       ${value.name}
-    //                   </label>
-    //               </div>
-    //               <button onclick="DelData(${value.id})"  id="taskdelete" class="task-delete"><i class="fa-solid fa-trash-can"></i></button>
-    //               </div>
-    //           `
-    //       )
-    //       .join("");
-    //   }
-    // });
+    bodyTasks();
   } else {
     if (page === "/login") {
       siteBody.innerHTML = ` 
